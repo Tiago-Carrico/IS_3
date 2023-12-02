887dd6b6e69a4bce63bedcf6e832d91513581309
Tiago Carriço <49901138+Tiago-Carrico@users.noreply.github.com>
2023-12-02 16:54:28 +0000

    purchase and sales, both classes and json functions are done and tested

--- demo/src/main/java/com/example/streams/KafkaStream.java
+++ demo/src/main/java/com/example/streams/KafkaStream.java
@@ -13,7 +13,9 @@ import org.apache.kafka.streams.kstream.Produced;
 
 public class KafkaStream {
     public static void main(String[] args) throws InterruptedException, IOException {
-  //String topicName = args[0].toString();
+    
+/*
+//String topicName = args[0].toString();
   String topicName = "bleh";
   String outtopicname = "resultstopicSales";
 
@@ -37,6 +39,38 @@ public class KafkaStream {
   streams.start();
   
   System.out.println("Reading stream from topic " + topicName);
-  
+  */
+
+
+    exercicio4();
+    
+    
+    }
+
+    public static void exercicio4(){
+      String topicName = "bleh";
+      String outtopicname = "resultstopicSales";
+
+      java.util.Properties props = new Properties();
+      props.put(StreamsConfig.APPLICATION_ID_CONFIG, "exercises-application2"); //saves the state, thats why the count is so high
+      props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "broker1:9092");
+      props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+      props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        
+      StreamsBuilder builder = new StreamsBuilder();
+      KStream<String> lines = builder.stream(topicName);
+
+      KTable<String, Long> outlines = lines.
+        groupByKey().count();
+
+      //outlines.toStream().to(outtopicname);
+      outlines.mapValues(v -> "" + v).toStream().to(outtopicname, Produced.with(Serdes.String(), Serdes.String())); 
+
+
+      KafkaStreams streams = new KafkaStreams(builder.build(), props);
+      streams.start();
+      
+      System.out.println("Reading stream from topic " + topicName);
+      
  }
 }
--- demo/src/main/java/com/example/templates/Purchase.java
+++ demo/src/main/java/com/example/templates/Purchase.java
@@ -1,5 +1,67 @@
 package com.example.templates;
 
+import java.math.BigDecimal;
+import org.json.JSONObject;
+
 public class Purchase {
-    
+    private BigDecimal price;
+    private int number;
+    private String type;
+    private int supplier;
+
+    public Purchase(){};
+
+    public Purchase(BigDecimal price, int number, String type, int supplier){
+        this.price = price;
+        this.number = number;
+        this.type = type;
+        this.supplier = supplier;
+    };
+
+    public void setPrice(BigDecimal price){
+        this.price = price;
+    }
+
+    public BigDecimal getPrice(){
+        return price;
+    }
+
+    public void setNumber(int number){
+        this.number = number;
+    }
+
+    public int getNumber(){
+        return number;
+    }
+
+    public void setType(String type){
+        this.type = type;
+    }
+
+    public String getType(){
+        return type;
+    }
+
+    public void setSupplier(int supplier){
+        this.supplier = supplier;
+    }
+
+    public int getSupplier(){
+        return supplier;
+    }
+
+    public String JsonToString(){
+        JSONObject obj = new JSONObject();
+
+        obj.put("price", this.price);
+        obj.put("number", this.number);
+        obj.put("type", this.type);
+        obj.put("supplier", this.supplier);
+
+        return obj.toString();  
+    }
+
+    public String toString(){
+        return ("{\nPrice: " + price + "\nNumber: " + number + "\nType: " + type + "\nSupplier: " + supplier + "\n}");
+    }
 }
